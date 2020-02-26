c5e1fa5c9ba40728583448fec2c878a91716aabf
许哲锋 <edwinxu@easyops.cn>
2020-02-26 10:13:00 +0800

    提交test

---
+++ test2.py
@@ -0,0 +1,36 @@
+#!/usr/local/python3/bin/python3
+# -*- coding: utf-8 -*-
+"""
+# @Author: edwin
+# date: 2020/1/16 4:25 下午
+"""
+
+import re
+# ret = re.match('a', 'abc')  # 同search,不过尽在字符串开始处进行匹配
+# print(ret)
+
+# import re
+# inputStr="hello python,ni hao c,zai jian python"
+# pat = re.compile("(hello python,ni hao) (\w+)(,zai jian python)")
+# print(pat.findall(inputStr))
+# # replaceStr=re.sub(r"(hello python,ni hao) (\w+)(,zai jian python)","\g<0>",inputStr)
+# # print(replaceStr)
+
+import os
+# config_file = "conf/nginx.conf"
+# config_dir, config_file = os.path.split(config_file)
+# config = get_path(config_dir, cwd) + "/" + config_file
+# config = 'aaaaaaaaNo such file'
+# config = 'aaaaaaaaNo suc没有那个文件或目录'
+config = 'aaaaaaaaNo suc'
+if 'No such file' in config or '没有那个文件或目录' in config:
+    print("进来了")
+    # config_dir, config_file = os.path.split(config)
+else:
+    config_dir = ''
+    config = ''
+# print("config_dir:", config_dir)
+# print("config_file:", config_file)
+
+
+print("test2")
